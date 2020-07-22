b925fa5d147c4450d4c44414507e8f571c306859
dadadayellow <2995015424@qq.com>
2020-07-22 12:53:55 +0800

    aa

--- CenterServer/app.py
+++ CenterServer/app.py
@@ -1,6 +1,8 @@
 from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api,Resource
+from flask import request
+import json
 
 #定义数据库连接地址
 SQLALCHEMY_DATABASE_URI='postgresql://postgres:0222@localhost:5432/test0'
@@ -24,7 +26,6 @@ class Camera(db.Model):
     __tablename__ = 'camera'
     cameraid=db.Column(db.Integer,primary_key=True)
     type=db.Column(db.VARCHAR(10))
-    status=db.Column(db.Boolean)
     ip=db.Column(db.VARCHAR(15),nullable=False)
     longitude=db.Column(db.Float)
     latitude = db.Column(db.Float)
@@ -35,46 +36,80 @@ class Rank_info(db.Model):
     cameraid=db.Column(db.Integer,db.ForeignKey('camera.cameraid'),primary_key=True)
 
 #测试函数
-class hello_world(Resource):
-    #从服务端get数据（有四个方法接口get，put，post，delete
-    def get(self):
-        #将数据库模型配置到数据库中
-        db.create_all()
-        #返回json数据
-        return User.query.get(2).username
-#将类函数放到相应url中
-api.add_resource(hello_world,'/')
+@app.route('/')
+def hello_world(Resource):
+    #将数据库模型配置到数据库中
+    db.create_all()
+    #返回json数据
+    t={
+        'data':User.query.get(2).username
+    }
+
+    return jsonify(t)
 
 
-#向普通用户返回摄像头信息
-class camera_map(Resource):
-    #从服务端get数据（有四个接口get，put，post，delete
-    def get(self,userid):
-        if Rank_info.query.filter_by(userid=userid).first()!=None:
 
-            t={
-                "error_code": 0,
-                "data":[
 
-                ]
+#监控摄像头地图页面
+@app.route('/index/camera_map_<int:userid>',methods=['GET','POST'])
+def camera_map(userid):
+    #向用户返回摄像头信息
+    if request.method=="GET":
+        #如果用户为管理员，则拥有所有监控权限
+        if User.query.get(userid).rank == 1:
+            t = {
+                'error_code': 0,
+                'data': []
             }
-            j=0
-            for i in Rank_info.query.all():
-                if i.userid == userid:
-                    t.data["cameraid"]=userid
-                    k=Camera.query.filter_by(cameraid=i.cameraid)
-                    t.data["longitude"]=k.longitude
-                    t.data["latitude"]=k.latitude
-                    t.data["permission"]=1
-                    j+=1
 
-                return jsonify(t)
+            for i in Camera.query.all():
+                t['data'].append({
+                    'cameraid':i.cameraid,
+                    'longitude':i.longitude,
+                    'latitude':i.latitude,
+                    'permission':1
+                    })
+            return jsonify(t)
+
+        #如果为普通用户则先查看有没有一个及以上摄像头权限
+        if Rank_info.query.filter_by(userid=userid).first()!=None:
+            t={
+                'error_code': 0,
+                'data':[]
+            }
+
+            for i in Camera.query.all():
+                #如果在rank_info表里有该用户的行数据，则有相应权限
+                if Rank_info.query.get((userid,i.cameraid))!=None :
+                    t['data'].append({
+                        'cameraid':i.cameraid,
+                        'longitude':i.longitude,
+                        'latitude':i.latitude,
+                        'permission':1
+                    })
+                else:
+                    t['data'].append({
+                        'cameraid': i.cameraid,
+                        'longitude': i.longitude,
+                        'latitude': i.latitude,
+                        'permission': 0
+                    })
+            return jsonify(t)
 
         else:
             return {"error_code": 1}
 
-#将类函数放到相应url中
-api.add_resource(camera_map,'/index/camera_map_<userid>')
+    #用户向server请求直播或录像视频
+    if request.method=="POST":
+        if request.form.get('type')=='live':
+
+
+            #code
+            return 'a'
+        elif request.form.get('type')=='video':
+            #code
+            return {'a':'b'}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
